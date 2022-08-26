0add5824fe19cf70e8fac445ec07b11c917af0dc
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-27 00:01:58 +0530

    Cards of Netflix Complete with css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
+import Series from "./Series";
+import Card from "./Card";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <h1>NETFLIX BEST SERIES IN 2022</h1>,
+    {Series.map(function Nflix(value) {
+  return (<>
+     <Card  
+      imgsrc={value.imgsrc}
+      title={value.title}
+      sname={value.sname}
+      link={value.link}/> 
+    </>
+  );
+})}
+  </>,
+  document.getElementById("root"));
